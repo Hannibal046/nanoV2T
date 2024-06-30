3158ea4503d9def739d4451b6b6ca37355d167f4
Hannibal046 <chengxin1998@stu.pku.edu.cn>
2024-06-30 12:11:23 +0000

    update

--- v2t/inference_from_trainer_ckpt.py
+++ v2t/inference_from_trainer_ckpt.py
@@ -10,6 +10,7 @@ import torch.nn.functional as F
 from tqdm import tqdm
 from accelerate import PartialState
 import torch.distributed as dist
+import transformers
 
 from v2t.utils import eval_v2t,write_jsonl,is_equal
 from v2t.data import collator,load_data
@@ -78,11 +79,39 @@ def maybe_gather_from_other_gpu(list_of_text,distributed_state):
             list_of_text = [x for y in all_ranks_objects for x in y]
     return list_of_text
 
+class GTREmbedder:
+    def __init__(self,device):
+        model_kwargs = {
+                "low_cpu_mem_usage": True,  # Not compatible with DeepSpeed
+                "output_hidden_states": False,
+            }
+        self.embedder = transformers.AutoModel.from_pretrained(
+            "sentence-transformers/gtr-t5-base", **model_kwargs,
+        ).encoder.to(device)
+        self.tokenizer = transformers.AutoTokenizer.from_pretrained(
+            "sentence-transformers/gtr-t5-base"
+        )
+        self.embedder.eval()
+    @torch.inference_mode()
+    def encode(self,list_of_text,**kwargs):
+        device = self.embedder.device
+        inputs = self.tokenizer(list_of_text,max_length=32,truncation=True,padding="max_length",return_tensors='pt').to(device)
+        model_output = self.embedder(input_ids = inputs.input_ids,attention_mask = inputs.attention_mask)
+        hidden_state = model_output.last_hidden_state
+        embeddings = self.mean_pool(hidden_state, inputs.attention_mask)
+        return embeddings
+    
+    def mean_pool(self,hidden_states, attention_mask):
+        B, S, D = hidden_states.shape
+        unmasked_outputs = hidden_states * attention_mask[..., None]
+        pooled_outputs = unmasked_outputs.sum(dim=1) / attention_mask.sum(dim=1)[:, None]
+        assert pooled_outputs.shape == (B, D)
+        return pooled_outputs
+    
 def parse_args():
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("--draft_dir")
-    parser.add_argument("--generator_name_or_path")
     parser.add_argument("--dataset_name_or_path")
     parser.add_argument("--embedder_name_or_path")
     parser.add_argument("--max_seq_length",type=int,default=32)
@@ -107,15 +136,47 @@ if __name__ == "__main__":
         "num_beams": args.num_beams,
         "do_sample": False,
         "no_repeat_ngram_size": 0,
-        "max_length": args.max_seq_length + 2, ## T5 would add <bos> and <eos>
+        "max_length": args.max_seq_length, ## T5 would add <bos> and <eos>
         "num_return_sequences":num_return_sequences,
     }
     if args.num_beam_groups > 1:
         generation_kwargs['num_beam_groups'] = args.num_beam_groups
         generation_kwargs['diversity_penalty'] = args.diversity_penalty
+    
     ## == load embedder and generator == ##
-    embedder = load_embedder(args.embedder_name_or_path).to(device)
-    generator,tokenizer = load_generator(args.generator_name_or_path,torch_compile=False)
+    
+    ##  =====MODIFY FOR OFFICIAL CHECKPOINT==== ##
+    embedder = GTREmbedder(device)
+    import vec2text
+    corrector_model = vec2text.models.CorrectorEncoderModel.from_pretrained("jxm/gtr__nq__32__correct")
+    generator,tokenizer = load_generator(
+        "google-t5/t5-base",
+        "sentence-transformers/gtr-t5-base",
+        expanding_factor=16,
+        torch_compile=False,
+        project_draft_embedding=True,
+        layer_norm_after_projection=True,
+        init_new_module=True
+    )
+
+    state_dict = corrector_model.state_dict()
+    keys = list(state_dict.keys())
+    for key in keys:
+        state_dict[key.removeprefix("encoder_decoder.")] = state_dict.pop(key)
+
+    keys = list(state_dict.keys())
+    for key in keys:
+        if "embedding_transform_1" in key:
+            state_dict[key.replace("embedding_transform_1","target_projector.projector")] = state_dict.pop(key)
+        if "embedding_transform_2" in key:
+            state_dict[key.replace("embedding_transform_2","diff_projector.projector")] = state_dict.pop(key)
+        if "embedding_transform_3" in key:
+            state_dict[key.replace("embedding_transform_3","draft_projector.projector")] = state_dict.pop(key)
+        if "layernorm" in key:
+            state_dict[key.replace("layernorm","proj_ln")] = state_dict.pop(key)
+    
+    generator.load_state_dict(state_dict,strict=True)
+    ##  =====MODIFY FOR OFFICIAL CHECKPOINT==== ##
     generator = generator.to(device).eval()
 
     ## == load dataset == ##
@@ -124,16 +185,27 @@ if __name__ == "__main__":
         tokenizer=tokenizer,
         partial_state = distributed_state,
         max_seq_length=args.max_seq_length,
-        embedder=embedder,
+        embedder=load_embedder("sentence-transformers/gtr-t5-base").to(device),
         load_split=['test'],
         draft_dir=args.draft_dir,
     )
     test_dataset = list(test_dataset)[:args.max_eval_samples]
+    for sample in test_dataset:
+        sample['text'] = tokenizer.decode(tokenizer(sample['text'],add_special_tokens=False,max_length=31,truncation=True).input_ids,skip_special_tokens=True)
+
     refs = [test_dataset[idx]['text'].strip() for idx in range(len(test_dataset))]
 
     ## == split dataset across multiple GPUs (if possible), also compatible with single GPU setting == ##
     with distributed_state.split_between_processes(test_dataset) as sharded_dataset:
         
+        ##  =====MODIFY FOR OFFICIAL CHECKPOINT==== ##
+        for idx,sample in enumerate(sharded_dataset):
+            sample['target_embedding'] = embedder.encode([sample['text']])[0]
+            draft_input_text = tokenizer.decode(sample['draft_input_ids'],skip_special_tokens=True)
+            sample['draft_input_ids'] = tokenizer("<pad>" + draft_input_text, max_length=32,truncation=True,padding=True,return_tensors='pt',add_special_tokens=False).input_ids[0]
+            sample['draft_embedding'] = embedder.encode([draft_input_text])[0]
+            
+
         ## core data structure, sample_id --> same sample with different draft
         dataset_dict = {}
         for sample in sharded_dataset:
@@ -197,7 +269,7 @@ if __name__ == "__main__":
                 assert len(dataset_dict[sample_id]) == len(id2hyps[sample_id])
                 
                 for rank in range(len(id2hyps[sample_id])):
-                    dataset_dict[sample_id][rank]['draft_input_ids'] = tokenizer(id2hyps[sample_id][rank]['hyp'],max_length=args.max_seq_length+1,truncation=True,padding=True,return_tensors='pt').input_ids[0]
+                    dataset_dict[sample_id][rank]['draft_input_ids'] = tokenizer("<pad>" + id2hyps[sample_id][rank]['hyp'],max_length=args.max_seq_length,truncation=True,padding=True,return_tensors='pt',add_special_tokens=False).input_ids[0]
                     dataset_dict[sample_id][rank]['draft_embedding'] = id2hyps[sample_id][rank]['hyp_embedding']
 
     ##== write back to disk ==##
